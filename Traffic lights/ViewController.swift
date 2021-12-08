5498b4de544b7878d59bfd2a99edc18d7f6ee1be
Vlad <vlad14196@gmail.com>
2021-12-08 23:53:06 +0300

    commit one

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -13,13 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
     
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+//        redView.isHidden = false
+//        yellowView.isHidden = false
+//        greenView.isHidden = false
+        redView.layer.cornerRadius    = 50
+        yellowView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius  = 50
+        startButton.layer.cornerRadius = 15
+        redView.alpha = 1
+        yellowView.alpha = 1
+        greenView.alpha = 1
 
-    @IBAction func stertButton(_ sender: Any) {
+        
     }
     
+    @IBAction func startPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redView.alpha == 1 && yellowView.alpha == 1 && greenView.alpha == 1 {
+            redView.alpha = 0.3
+            
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            redView.alpha = 1
+            
+            
+        } else if redView.alpha == 1 {
+            greenView.alpha = 0.3
+            yellowView.alpha = 1
+            
+        }
+    }
 }
 
