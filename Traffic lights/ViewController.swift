553f477274393f95116a2bd837b2ff5a601db4db
Vlad <vlad14196@gmail.com>
2021-12-08 21:44:42 +0300

    colorViews

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func stertButton(_ sender: Any) {
+    }
+    
 }
 
